e913e93a0aa39c3fe8f60e549973695f185ad4be
Carmofrasao <anderfrasao52@gmail.com>
2022-06-09 00:06:28 -0300

    implementar grade

--- App.js
+++ App.js
@@ -8,11 +8,12 @@ let autos = '';
 
 for(let l = 0; l < 75; l++){
     for(let i = 0; i < 75; i++){
+        // criar grade 75x75, cada div é um quadrado
         const grade = document.createElement('div');
         grade.classList.add('grade');
         divs.appendChild(grade);
     }
     autos += 'auto '; 
 }
-
+// dividir a grade em colunas, cada 'auto é uma coluna'
 divs.style.gridTemplateColumns=autos;
