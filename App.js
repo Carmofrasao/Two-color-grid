6bdb076d4ccd54774b9e0323dec03396adffcd32
Carmofrasao <anderfrasao52@gmail.com>
2022-06-13 17:28:40 -0300

    feito

--- App.js
+++ App.js
@@ -7,16 +7,17 @@ let autos = '';
 let tam_grade = 0;
 
 btn_grade.addEventListener('click', () => {
-    for(child of divs.children){
-        divs.removeChild(child);
+    while (divs.firstChild) {
+        divs.removeChild(divs.firstChild);
     }
+    
     tam_grade = prompt("Digite o tamanho da grade:");
     for(let l = 0; l < tam_grade; l++){
         for(let i = 0; i < tam_grade; i++){
             // criar grade, cada div é um quadrado
             const grade = document.createElement('div');
             grade.classList.add('grade');
-            grade.onmouseover=()=>{
+            grade.onmouseover = () => {
                 grade.style.border = "2px solid purple";
             };
             divs.appendChild(grade);
@@ -26,4 +27,5 @@ btn_grade.addEventListener('click', () => {
 
     // dividir a grade em colunas, cada 'auto' é uma coluna
     divs.style.gridTemplateColumns=autos;
+    autos = '';
 })
